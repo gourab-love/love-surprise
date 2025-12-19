b4a9aac4faf9f901464de856846852561c08d006
gourab-love <gourabghosh1432@gmail.com>
2025-12-20 00:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    const text = document.getElementById("text");
+    const tree = document.getElementById("tree");
+
+    let i = 0;
+
+    function showText() {
+      text.innerText = data.messages[i];
+      i++;
+
+      if (i < data.messages.length) {
+        setTimeout(showText, 3500);
+      } else {
+        setTimeout(() => {
+          tree.classList.remove("hidden");
+        }, 3000);
+      }
+    }
+
+    showText();
+  });
 
